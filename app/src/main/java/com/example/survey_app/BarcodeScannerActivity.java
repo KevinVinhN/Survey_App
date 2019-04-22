3114687fef4c082a606a8405bf318dbc1ba4bf04
Kevin <kevinvinhn@gmail.com>
2019-04-22 15:05:38 -0400

    First Push

---
+++ app/src/main/java/com/example/survey_app/BarcodeScannerActivity.java
@@ -0,0 +1,57 @@
+package com.example.bookies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.survey_app.PickStoreAndScan;
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class BarcodeScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    ZXingScannerView scannerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        scannerView = new ZXingScannerView(this);
+        setContentView(scannerView);//set instead of R.layout.activity_scanner
+
+    }
+
+    @Override
+    public void handleResult(Result result) {
+
+        String text;
+        if(result == null) {
+            text = "No barcode scanned";
+        }
+        else{
+            if(!result.getBarcodeFormat().toString().equalsIgnoreCase("EAN_13")){
+                text = "Barcode is not an ISBN";
+            }
+            else{
+                text = result.getText();
+            }
+
+        }
+        PickStoreAndScan.surveyEditText.setText(text);
+        onBackPressed();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        scannerView.stopCamera();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        scannerView.setResultHandler(this);
+        scannerView.startCamera();
+    }
+}
